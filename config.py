4fa36ea2813584e1a2988b2587c19b4d107df276
lorenzoyt22 <lorenzoyt22@gmail.com>
2025-07-14 14:16:00 +0200

    Update and rename config.py to config.txt

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")  # devi impostare questa variabile su Railway
-CHAT_IDS = list(map(int, os.getenv("CHAT_IDS", "").split(',')))  # es: "12345678,87654321"
-
-
