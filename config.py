c150e626ff6de6b1695c0a1a41f026ffc8dc7f8e
lorenzoyt22 <lorenzoyt22@gmail.com>
2025-07-14 13:16:44 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+TELEGRAM_TOKEN = 'INSERISCI_IL_TUO_BOT_TOKEN'
+CHAT_IDS = ['CHAT_ID_1', 'CHAT_ID_2']  # Lista chat o gruppi da notificare
+SCHEDULE_TIME = "07:00"
+
+RSS_FEEDS = {
+    "CoinDesk": "https://www.coindesk.com/arc/outboundfeeds/rss/",
+    "The Block": "https://www.theblock.co/latest-crypto-news?format=rss",
+    "Decrypt": "https://decrypt.co/feed"
+}
+
+MACRO_KEYWORDS = ["Fed", "FOMC", "dollar", "Trump", "dazi", "Cina", "Europa", "UE"]
+CRYPTO_KEYWORDS = ["crypto", "bitcoin", "ethereum", "SEC", "ETF", "blockchain"]
