b9b6cb420813ad17f5555cb37fdd9295af7bf7ab
lorenzoyt22 <lorenzoyt22@gmail.com>
2025-07-14 13:29:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-TELEGRAM_TOKEN = 'INSERISCI_IL_TUO_BOT_TOKEN'
-CHAT_IDS = ['CHAT_ID_1', 'CHAT_ID_2']  # Lista chat o gruppi da notificare
+import os
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')  # legge il token dalla variabile d'ambiente
+CHAT_IDS = os.getenv('CHAT_IDS', '').split(',')  # es: "123456789,-987654321" diventa lista
 SCHEDULE_TIME = "07:00"
 
 RSS_FEEDS = {
